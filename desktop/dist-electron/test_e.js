5f152968424c9c83fea54bb12bb5a18380612a59
Shihabul Hasan Shihab <6shihab@gmail.com>
2026-04-12 06:19:44 +0600

    Delete desktop/dist-electron directory

--- desktop/dist-electron/test_e.js
+++
@@ -1,9 +0,0 @@
-const e = require('electron');
-console.log('type:', typeof e);
-if (typeof e === 'object') {
-  console.log('keys:', Object.keys(e || {}).slice(0, 10).join(', '));
-  console.log('app:', typeof e.app);
-} else {
-  console.log('value:', String(e).slice(0, 80));
-}
-process.exit(0);
